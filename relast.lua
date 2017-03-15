818cd41d0998fb4c6de5f8490738d10d8b826dcc
MechaLynx <MechaLynx@users.noreply.github.com>
2017-03-16 01:17:09 +0200

    Removed description

--- relast.lua
+++ relast.lua
@@ -1,8 +1,3 @@
--- Relast turns on "loop one" if the playlist starts playing
--- the last media item. The idea is that you have something playing
--- in the background and when you want to switch, whatever you add
--- gets to be played continuously until you add something else.
---
 -- Implementation details:
 --
 -- You don't get the current playlist index from vlc.playlist.current()
